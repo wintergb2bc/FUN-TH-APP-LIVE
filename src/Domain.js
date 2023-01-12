c0d908132c2e1e83e10bbb1b658286a33617d817
winter <luguibin123@gmail.com>
2023-01-12 18:51:55 +0800

    update

---
+++ src/Domain.js
@@ -0,0 +1,57 @@
+
+// live
+
+window.common_url = 'https://gatewayfun88th.gamealiyun.com'
+CodePushKeyIOS = '4pE0MVZb36c2abnNUwWD21QiAsnB4ksvOXqog'
+CodePushKeyAndroid = 'kbTCfHBFwY3zByaGKmF6xR95Q8Di4ksvOXqog'
+ //要和codepush的appName對上，用來防止推錯，只有LIVE代碼需要配置
+ window.CodePushAppNameIOS = "F1M2-Live-IOS";
+ window.CodePushAppNameAndroid = "F1M2-Live-android";
+
+
+window.appAndroidId = "com.F1M2LIVE.soexample";
+window.appIOSId = "com.F1uM2uTH.LIVE";
+
+
+
+//-----IM配置------
+//测试
+window.IMAccessCode = 'ca9fb2a3e194173a', //fun測試
+    window.IMApi = 'https://gatewayimvnstaging.bbentropy.com/api/mobile/' //fun測試 proxy服
+//-----緩存API配置-----
+window.CacheApi = 'https://sapivn.letiyu88.com', //測試環境
+    //游戏icon
+    SportImageUrl = 'https://simg.letiyu88.com/'
+//推送
+SignalRUrl = 'https://gatewaystagingfun88wsnotification1.gamealiyun.com'
+//歐洲杯相關的聯賽ID
+window.seasonId = 24 // 賽季, 23 = 2016 , 24 = 2020 歐洲杯
+window.EuroCup2021LeagueIds = [20161]
+//歐洲杯比賽時間
+window.EuroCup2021FirstEventTime = '2021-06-11T15:00:00.0000000-04:00' //第一場
+window.EuroCup2021FinalEventTime = '2021-07-11T15:00:00.0000000-04:00' //最後一場
+//倒計時結束時間
+window.EuroCup2021CountDownEndTime = '2021-06-12T00:00:00.0000000+07:00' //12 June 2021 00:00:00 GMT +8
+window.EuroCupDomain = 'https://shsbwkpf.funpo.com:2041/sports-data-gateway'//排名api
+
+
+
+//线上
+//-----IM配置------
+// window.IMAccessCode = '2cc03acc80b3693c', //fun測試
+// window.IMApi = 'https://gatewayimvn.bbentropy.com/api/mobile/' //fun測試 proxy服
+// //-----緩存API配置-----
+// window.CacheApi = 'https://sapivn.leyouxi211.com', //測試環境
+// //游戏icon
+// SportImageUrl = 'https://simg.letiyu88.com/'
+// //推送
+// SignalRUrl = 'https://gatewaystagingfun88wsnotification1.gamealiyun.com'
+// //歐洲杯相關的聯賽ID
+// window.seasonId = 24 // 賽季, 23 = 2016 , 24 = 2020 歐洲杯
+// window.EuroCup2021LeagueIds = [20161]
+// //歐洲杯比賽時間
+// window.EuroCup2021FirstEventTime = '2021-06-12T00:00:00.0000000-04:00' //第一場
+// window.EuroCup2021FinalEventTime = '2021-07-11T15:00:00.0000000-04:00' //最後一場
+// // 倒計時結束時間
+// window.EuroCup2021CountDownEndTime = '2021-06-12T00:00:00.0000000+07:00' //12 June 2021 00:00:00 GMT +8
+// window.EuroCupDomain = 'https://shsbwkpf.funpo.com:2041/sports-data-gateway'//排名api
